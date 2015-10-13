2e5fd66cbc27e80112a147e70e6b20c55a74fd50
Ludvig Nilsson <LudvigNilsson@wlanguest786.eduroam.hj.se>
2015-10-13 11:54:44 +0200

    produktsida

---
+++ wp-content/themes/projekt/product.php
@@ -0,0 +1,96 @@
+<html>
+<head>
+	<title>Produkt</title>
+	<meta charset="utf-8">
+	<link href="style.css" rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="js.js"></script>
+</head>
+<body>
+
+
+<div id="utfyllnad"></div>
+
+
+
+<div id="pagecontainer">
+
+		<div id="product">
+
+			<div id="left">
+				<div id="display">
+					<div id="previous"></div>
+					<div id="next"></div>
+					<img id="mainImg" src="img/byxa1.jpg">
+					<ul id="altImg">
+						<li id="one" class="altImgs"><img src="img/byxa1.jpg"></li>
+						<li id="two" class="altImgs"><img src="img/byxa2.jpg"></li>
+						<li id="three" class="altImgs"><img src="img/byxa3.jpg"></li>
+						<li id="four" class="altImgs"><img src="img/byxa4.jpg"></li>
+					</ul>
+				</div>
+
+			</div>
+
+			<div id="right">
+					<div id="info">
+						<h1>Namn<br>Produkt</h1>
+						<p id="description">Det här är ett par byxor som du kan ha på benen ifall du känner för det. Du skulle även kunna dra dem över huvudet om det skulle kännas bättre för dig. Det viktiga är att du köper byxorna så att vi kan ta dina pengar.</p>
+						<br>
+						<p>Storlek</p>
+							<select id="size">
+							  <option value="28-32">28-32</option>
+							  <option value="28-34">28-34</option>
+							  <option value="29-32">29-32</option>
+							  <option value="29-34">29-34</option>
+							  <option value="30-32">30-32</option>
+							  <option value="30-34">30-34</option>
+							  <option value="31-34">31-34</option>
+							  <option value="31-34">31-34</option>
+							  <option value="32-32">32-32</option>
+							  <option value="32-34">32-34</option>
+							  <option value="33-32">33-32</option>
+							  <option value="33-34">33-34</option>
+							  <option value="34-32">34-32</option>
+							  <option value="34-34">34-34</option>
+							  <option value="36-32">36-32</option>
+							  <option value="36-34">36-34</option>
+							</select>
+						<br>
+						<h3>Pris</h3>
+							<select id="quantity">
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+							</select>
+						<br>
+						<input id="add" type="submit" value="Lägg i varukorg">
+					</div>
+
+			</div>
+
+		</div>
+
+	<div id="related">
+		<h2>Relaterat</h2>
+			<ul id="relatedLinks">
+				<li><a href=""><img src="img/byxa1.jpg"></a></li>
+				<li><a href=""><img src="img/byxa1.jpg"></a></li>
+				<li><a href=""><img src="img/byxa1.jpg"></a></li>
+			</ul>
+	</div>
+
+
+</div>
+
+<div id="utfyllnad2"></div>
+
+
+<script type="text/javascript" src="js.js"></script>
+
+
+
+</body>
+</html>
