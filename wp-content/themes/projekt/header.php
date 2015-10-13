d8446a8af797f5e5b943fc810f90ed90772c1638
escarokokolollol <oscar_ek21@hotmail.com>
2015-10-13 11:11:31 +0200

    HEJ 1112

--- wp-content/themes/projekt/header.php
+++ wp-content/themes/projekt/header.php
@@ -1,20 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Torkad frukt! 2.0</title>
+	<title>Jeansshopen</title>
 	<meta charset="utf-8"/>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:700,500,300' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"/>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+
 
 </head>
 <body>
-
 	<header>
-		<h1>Min wp-titel</h1>
-		<nav>
-			<ul>
-				<li><a href="#">HEM</a></li>
-				<li><a href="#">OM</a></li>
-				<li><a href="#">KONTAKT</a></li>
-			</ul>	
-		</nav>
+		<img src="wp-content/themes/projekt/images/logotest.svg">
+		<h1>Jeansshopen</h1>
+
+		
 	</header>
+	<nav>
+		<ul>
+			<li><a href="#">Herr</a></li>
+			<li><a href="#">Dam</a></li>
+
+			<div class="box">
+			  <div class="container-1">
+			      <span class="icon"><i class="fa fa-search"></i></span>
+			      <input type="search" id="search" placeholder="Search..." />
+			  </div>
+			</div>
+
+		</ul>	
+	</nav>
+
+
+
