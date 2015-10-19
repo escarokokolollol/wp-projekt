451782b991dc36eb3a48e0443dfd274bb27b776c
Ludvig Nilsson <LudvigNilsson@wlanguest139121.eduroam.hj.se>
2015-10-19 13:13:40 +0200

    homepage

---
+++ wp-content/themes/projekt/homepage.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+	<title>start</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,900,800,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,700,900' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Lato:100,300' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,700,800,900' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+
+
+
+
+
+<div id="container">
+
+	<div id="top">
+		<div class="main_landing">
+			<span class="pre_header">Hej hallå där</span>
+			<span class="main_header">Höstnyheter</span>
+		</div>
+		<img id="top_img" src="wp-content/themes/projekt/images/NewLook-FW13-Denim.jpg">
+	</div>
+
+	<ul id="content">
+		<li>
+			<div>
+				<span class="span_upper_left">Du vill verkligen ha hennes jeans</span>
+				<div class="layer"></div>
+				<img src="wp-content/themes/projekt/images/J.Crew-Denim-2014-Collection-1.jpg">
+			</div>
+		</li>
+		<li>
+			<div>
+				<span class="span_lower_right">Sås är en snygg färg</span>
+				<div class="layer"></div>
+				<img src="wp-content/themes/projekt/images/denim.png">
+			</div>
+		</li>
+		<li>
+			<div>
+				<span class="span_lower_right">Roadtrippin' utan karta</span>
+				<div class="layer"></div>
+				<img src="wp-content/themes/projekt/images/NewLook-FW13-Denim.jpg">
+			</div>
+		</li>
+		<li>
+			<div>
+				<span class="span_lower_right">Hundmaten var för god</span>
+				<div class="layer"></div>
+				<img src="wp-content/themes/projekt/images/models.png">
+			</div>
+		</li>
+		<li>
+			<div>
+				<span class="span_lower_left">Hon kan inte köra bil</span>
+				<div class="layer"></div>
+				<img src="wp-content/themes/projekt/images/car.png">
+			</div>
+		</li>
+		<li>
+			<div>
+				<span class="span_lower_left">Han ser kul ut</span>
+				<div class="layer"></div>
+				<img src="wp-content/themes/projekt/images/denommodel.png">
+			</div>
+		</li>
+	</ul>
+
+</div>
+
+
+
+
+</body>
+</html>
