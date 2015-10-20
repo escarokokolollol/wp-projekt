4f046e2ebe814bc42b67b7f69e5641c41780492a
Ludvig Nilsson <LudvigNilsson@wlanguest142120.eduroam.hj.se>
2015-10-20 15:07:29 +0200

    hej

--- wp-content/themes/projekt/index.php
+++ wp-content/themes/projekt/index.php
@@ -1,15 +1,20 @@
 <?php get_header() ?>
 
 
-<!-- produktvy -->
+
 
 	<?php if( have_posts() ): while( have_posts() ): the_post(); ?>
 		<article>
+
+
+			<a href=""><img src="<?php ?>"><h1><?php ?></h1></a>
+
+
+
 			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<p><?php the_content(); ?></p>
 		</article>
 	<?php endwhile; endif; ?>
-
 
 
 
